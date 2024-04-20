4b26aa5da6db9bee95c95715259681c3c259f769
herminioneto <herminio_neto@outlook.com>
2024-04-20 17:23:07 -0300

    Changed list_post to function based view

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,6 @@ router.register(r"users", viewset=views.UserViewSet, basename="user")
 urlpatterns = [
     path("", include(router.urls)),
     path('api-auth/', include("rest_framework.urls", namespace="rest_framework")),
-    path("posts/", views.ListPost.as_view())
+    path("posts/", views.list_post)
 ]
 
--- social_network/views.py
+++ social_network/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
 
-from social_network.models import Post, User
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from social_network.models import Post, User
 from social_network.serializers import PostSerializer, UserSerializer
 
-class ListPost(APIView):
-    def get(self, request, format=None):
+@api_view(['GET'])
+def list_post(request, format=None):
+    if request.method == 'GET':
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
